4be08d91aeedd5987a65fc156d2bd66a0eaf29f3
fadidebow <34662511+fadidebow@users.noreply.github.com>
2021-04-03 23:42:33 +0300

    Initial commit

---
+++ nav.php
@@ -0,0 +1,55 @@
+  <div id="topbar" class="d-none d-lg-flex align-items-center fixed-top ">
+    <div class="container d-flex align-items-center">
+      <div class="contact-info mr-auto">
+        <ul>
+          <li><i class="icofont-envelope"></i><a href="mailto:tr5@bacttraining.com">tr5@bacttraining.com</a></li>
+          <li><i class="icofont-phone"></i>+97125325888</li>
+          <li><i class="icofont-clock-time icofont-flip-horizontal"></i> Sat-Thur 9am - 8pm</li>
+        </ul>
+      </div>
+      <div class="cta">
+        <a href="#about" class="scrollto">Lets Go</a>
+      </div>
+    </div>
+  </div>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top ">
+    <div class="container d-flex align-items-center">
+
+      <h1 class="logo mr-auto"><a href="#header" class="scrollto"><img src="assets/img/ff.jpg" style="width: 70px;height: 120px;"> CitizenShips</a></h1>
+    
+
+      <nav class="nav-menu d-none d-lg-block">
+        <ul>
+          <li class="active"><a href="index.php">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#services">Services</a></li>
+          
+          <li><a href="#team">Team</a></li>
+          
+          <li><a href="blog.html">Blog</a></li>
+          <li class="drop-down"><a href="">Countries</a>
+            <ul>
+              <li><a href="#">Drop Down 1</a></li>
+              <li class="drop-down"><a href="#">Deep Drop Down</a>
+                <ul>
+                  <li><a href="#">Deep Drop Down 1</a></li>
+                  <li><a href="#">Deep Drop Down 2</a></li>
+                  <li><a href="#">Deep Drop Down 3</a></li>
+                  <li><a href="#">Deep Drop Down 4</a></li>
+                  <li><a href="#">Deep Drop Down 5</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Drop Down 2</a></li>
+              <li><a href="#">Drop Down 3</a></li>
+              <li><a href="#">Drop Down 4</a></li>
+            </ul>
+          </li>
+          <li><a href="#contact">Contact Us</a></li>
+
+        </ul>
+      </nav><!-- .nav-menu -->
+
+    </div>
+  </header>
